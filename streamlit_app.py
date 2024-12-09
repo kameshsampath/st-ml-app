77684c5a47bee124fe7f4f095bd5f6ee3ab755f1
Kamesh Sampath <kamesh.sampath@hotmail.com>
2024-12-09 08:20:45 +0530

    expand data expander

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,12 @@
-import streamlit as st
-
 # import pandas to read the our data file
 import pandas as pd
+import streamlit as st
 
 st.title("🤖 Machine Learning App")
 
 st.write("Welcome to world of Machine Learning with Streamlit.")
 
-with st.expander("Data"):
+with st.expander("Data", expanded=True):
     st.write("**Raw Data**")
     # read the csv file
     df = pd.read_csv("data/penguins_cleaned.csv")
